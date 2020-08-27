42e101addaf16b24ec313d5e4f1ad8416c07cd45
Yannick Bourenane <yannick.bourenane@gmail.com>
2020-08-27 15:29:30 +0200

    Angular basics components + routing

---
+++ src/app/home/home.component.ts
@@ -0,0 +1,18 @@
+import { Component, OnInit } from "@angular/core";
+
+@Component({
+  selector: "app-home",
+  templateUrl: "./home.component.html",
+  styleUrls: ["./home.component.css"],
+})
+export class HomeComponent implements OnInit {
+  h1Style: boolean = false;
+
+  constructor() {}
+
+  ngOnInit() {}
+
+  firstClick() {
+    this.h1Style = !this.h1Style;
+  }
+}
